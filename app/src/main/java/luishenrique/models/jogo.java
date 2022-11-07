b061bf828f9e341856ecf81329e3f9a1047b6ca1
luis10g <joicefernandasouzaoliveira@gmail.com>
2022-11-07 17:07:34 +0000

    Prova

---
+++ app/src/main/java/luishenrique/models/jogo.java
@@ -0,0 +1,26 @@
+package luishenrique.models;
+
+public class Jogo {
+    private int id;
+    private String titulo;
+
+    private int idGenero;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return this.id;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public int getTitulo() {
+        return this.titulo;
+    }
+    public void setIdGenero(int idGenero) {
+        this.idGenero = idGenero;
+    }
+    public int getIdGenero() {
+        return this.idGenero;
+}
